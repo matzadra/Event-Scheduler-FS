aedd685883e3484e62e6d9e5b6b0aa5c98180f7d
matzadra <matheuszadra@gmail.com>
2025-01-09 00:36:10 -0300

    chore: configure environment variables

--- backend/src/config/ormconfig.ts
+++ backend/src/config/ormconfig.ts
@@ -2,12 +2,12 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: '172.22.224.1',
-  port: 5432,
-  username: 'postgres',
-  password: 'eventpass123',
-  database: 'event_scheduler',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_DATABASE || 'test',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: true,
-  logging: true,
+  synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+  logging: process.env.TYPEORM_LOGGING === 'true',
 };
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,9 +8,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    origin: process.env.CORS_ORIGIN || 'http://localhost:3001',
+    methods: process.env.CORS_METHODS || 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: process.env.CORS_CREDENTIALS === 'true',
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
